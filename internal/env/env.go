64752e519d1883de9cbc27f7ee20d6baf96c4480
Tom Richards <tom@tomrichards.net>
2025-07-09 18:22:11 -0400

    Remove fetch

--- internal/env/env.go
+++ internal/env/env.go
@@ -15,15 +15,3 @@ func BypassCache() bool {
 func Production() bool {
 	return strings.EqualFold(os.Getenv("APP_ENV"), "production")
 }
-
-// Fetch retrieves the environment variable with the given name,
-// or returns the given default value if the variable is not set.
-func Fetch(name, defaultValue string) string {
-	value, ok := os.LookupEnv(name)
-
-	if ok {
-		return value
-	}
-
-	return defaultValue
-}
